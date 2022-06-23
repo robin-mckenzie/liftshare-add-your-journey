5a8de4c4a1686c62a42603896afbecbe80c00a89
Robin McKenzie <robin@agilis.ai>
2022-06-23 07:50:56 +0100

    Updates form from basic bootstrap one

--- src/components/AddJourneyForm.tsx
+++ src/components/AddJourneyForm.tsx
@@ -8,21 +8,20 @@ const AddJourneyForm = () => {
       <p>Whether you're looking to Liftshare as a driver or a passenger, 
         listing your journey is the best way to find a match.</p>
       <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+        <Form.Group className="mb-2">
+          <Form.Label>Journey start</Form.Label>
+          <Form.Control id="originText" type="text" placeholder="e.g. Postcode, Street Name" />
+          <Form.Control id="originLat" type="text" placeholder="[HIDDEN] Latitude"  />
+          <Form.Control id="originLng" type="text" placeholder="[HIDDEN] Longitude" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+        <Form.Group className="mb-2">
+          <Form.Label>Destination</Form.Label>
+          <Form.Control id="destinationText" type="text" placeholder="e.g. Postcode, Street Name" />
+          <Form.Control id="destinationLat" type="text" placeholder="[HIDDEN] Latitude"  />
+          <Form.Control id="destinationLng" type="text" placeholder="[HIDDEN] Longitude" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
